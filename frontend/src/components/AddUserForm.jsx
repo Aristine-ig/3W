9270d50b43d677d2fea28de1d1e10d77b4eb63a3
Aristine-ig <aristine277@gmail.com>
2025-07-19 15:10:27 +0530

    commit

--- frontend/src/components/AddUserForm.jsx
+++ frontend/src/components/AddUserForm.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 
 const API_BASE_URL = 'http://localhost:5000/api'
+// const API_BASE_URL = 'https://threew-frontend-ybsj.onrender.com/api'
+
+
 
 const AddUserForm = ({ onUserAdded }) => {
   const [name, setName] = useState('')
